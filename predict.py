ea8146ba15d58f8eb0612fc688b9b47839c92416
ashu-kamrip <ashu.adhana@gmail.com>
2020-10-08 13:21:01 +0530

    made small changes

--- predict.py
+++ predict.py
@@ -65,7 +65,7 @@ def predict_fn(input_data, model):
     if model.word_dict is None:
         raise Exception('Model has not been loaded properly, no word_dict.')
     
-    # TODO: Process input_data so that it is ready to be sent to our model.
+    # Process input_data so that it is ready to be sent to our model.
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
@@ -95,7 +95,7 @@ def predict_fn(input_data, model):
     # Make sure to put the model into evaluation mode
     model.eval()
 
-    # TODO: Compute the result of applying the model to the input data. The variable `result` should
+    #Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
     with torch.no_grad():
         output = model.forward(data)
